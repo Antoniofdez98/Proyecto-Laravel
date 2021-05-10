27906b5991b94c1c6aed27fb340d21d41d65cd96
Antonio <antoniofdez98@gmail.com>
2021-05-10 17:56:35 +0200

    Mostrar tabla de heroes

--- app/Http/Controllers/HeroController.php
+++ app/Http/Controllers/HeroController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Hero;
 
 class HeroController extends Controller
 {
     public function index(){
-        return view('admin.heroes.index');
+
+        $heroes = Hero::all();
+
+        return view('admin.heroes.index', ['heroes' => $heroes]);
     }
 }
